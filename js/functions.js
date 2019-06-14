71070ddf3e973d6f5d34fea75e4cf2b997d153e9
rlovelessiii <richardlovelessiii@gmail.com>
2019-06-14 17:25:40 -0400

    Updated everything, styling completed, config.json updated

--- js/functions.js
+++ js/functions.js
@@ -3,37 +3,67 @@
 NUMSTEPS = 12;
 
 $(document).ready(() => {
-    console.log("document is ready");
-    $.getJSON('config.json', initSteps);
+    // Initialize the instructions from the config file
+    $.getJSON('config.json', init);
+
+    // Events
     $("#carousel").carousel('pause')
         .on('slide.bs.carousel', event => {
             $('#step' + event.to).collapse('show')
-        });
+    }).on('slid.bs.carousel', () => {
+        $('#carousel').carousel('pause')
+    });
 });
 
-initSteps = (data) => {
-    $('#title').html(data.info.title);
-    $.each(data.steps, (key, value) => {
+init = async (data) => {
+    await initInfo(data.info);
+    await initSteps(data.steps);
+    await initSlides(data.images);
+};
+
+initInfo = (info) => {
+    $('#title').html(info.title);
+    $('#desc').html(info.description);
+    $.each(info.materials, (key, value) => {
+        $('#materials').append($('<li>' + value + '</li>'))
+    });
+    $.each(info.warnings, (key, value) => {
+        $('#warnings').append($('<li>' + value + '</li>'))
+    });
+    $.each(info.authors, (key, value) => {
+        $('#authors').append($('<li>' + value + '</li>'))
+    })
+};
+
+
+initSteps = (steps) => {
+    $.each(steps, (key, value) => {
         $("#step-list")
             .append(
                 $('<div class="card">')
                     .append(
                         $('<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#step' + key + '"></button>')
-                            .append($('<h5><strong>Step ' + key + '</strong></h5>'))
+                            .append($('<h5>' + value.title + '</h5>'))
                             .on('click', () => {
-                                $('#carousel').carousel(parseInt(key));
+                                $('#carousel').carousel(parseInt(key)).carousel('pause');
                             }))
                     .append(
                         $('<div class="collapse" id="step' + key + '" data-parent="#step-list"></div>')
                             .append(
                                 $('<div class="card-body"></div>')
-                                    .html(value)
+                                    .html(value.details)
                             )
                     )
             )
     });
 };
 
-showSlide = (slide) => {
-    $("#carousel").carousel(slide)
+initSlides = (images) => {
+    $.each(images, (key, value) => {
+        $('#slides').append(
+            $('<div class="carousel-item"></div>').append(
+                $('<img src="assets/'+ value + '" class="d-block w-100">')
+            )
+        )
+    })
 };
